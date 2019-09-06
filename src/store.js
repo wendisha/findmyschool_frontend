14a8d00a865e700ef171039e548c1f5f44a78390
Wendy Calderon <wendycalderon@gmail.com>
2019-09-06 17:56:46 -0400

    Import currentUser reducer into store.js

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import usersReducer from './reducers/users.js'
+import currentUser from './reducers/currentUser.js'
 import thunk from 'redux-thunk'
 
-const reducer = combineReducers({ user: usersReducer });
+//Displaying different syntax; could've user currentUser: currentUser
+const reducer = combineReducers({ user: usersReducer, currentUser });
 
 // Newer way of setting up middleware
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
