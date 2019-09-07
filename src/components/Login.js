6db2ce1533ff20f2e101ddf0f1a237527a123c46
Wendy Calderon <wendycalderon@gmail.com>
2019-09-06 23:42:41 -0400

    Set up sending requests to the backend

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { updateLoginForm } from "../actions/loginForm.js"
+import { login } from "../actions/currentUser.js"
 
 //props get passed into a functional component as an object argument
-const Login = ({ loginForm, updateLoginForm }) => {
+const Login = ({ loginFormData, updateLoginForm, login }) => {
     const handleInputChange = event => {
         const { name, value } = event.target
         const updatedFormInfo = {
-          ...loginForm,
+          ...loginFormData,
           [name]: value
         }
         updateLoginForm(updatedFormInfo)
     }
 
+    const handleSubmit = event => {
+        event.preventDefault()
+        login(loginFormData)
+    }
+
     return (
-        <form onSubmit={undefined}>
-          <input placeholder="username" value={ loginForm.username } name="username" type="text" onChange={ handleInputChange } />
-          <input placeholder="password" value={ loginForm.password } name="password" type="text" onChange={ handleInputChange } />
+        <form onSubmit={handleSubmit}>
+          <input placeholder="username" value={loginFormData.username} name="username" type="text" onChange={ handleInputChange } />
+          <input placeholder="password" value={loginFormData.password} name="password" type="text" onChange={ handleInputChange } />
           <input type="submit" value="Log In"/>
         </form>
     )
@@ -25,10 +31,10 @@ const Login = ({ loginForm, updateLoginForm }) => {
 //mapstatetoprops gives us access to chunks of state from the store as props, that we can use as an argument to this component in the form of an object
 const mapStateToProps = state => {
     return {
-      loginForm: state.loginForm
+      loginFormData: state.loginForm
     }
 }
 
 //Connect is a function that takes up to 4 arguments, and returns a function that takes a component and returns a component
 //updateLoginForm refers to simplified syntax, representing a key value pair with the same name
-export default connect(mapStateToProps, { updateLoginForm } )(Login)
+export default connect(mapStateToProps, { updateLoginForm, login } )(Login)
