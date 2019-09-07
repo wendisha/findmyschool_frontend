f3fc813cd2159bef1ad115c604e0b09a916e90fc
Wendy Calderon <wendycalderon@gmail.com>
2019-09-06 22:01:06 -0400

    Give value of undefined for testing purposes

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <form onSubmit={}>
-          <input placeholder="username" value={} name="username" type="text" onChange={} />
-          <input placeholder="password" value={} name="password" type="text" onChange={} />
+        <form onSubmit={undefined}>
+          <input placeholder="username" value={undefined} name="username" type="text" onChange={undefined} />
+          <input placeholder="password" value={undefined} name="password" type="text" onChange={undefined} />
           <input type="submit" value="Log In"/>
         </form>
     )
