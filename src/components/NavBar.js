bf1a5b2226f584dc62ce567fa4c5a9db98109cd5
Wendy Calderon <wendycalderon@gmail.com>
2019-09-07 19:37:15 -0400

    Fix component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import Login from './Login'
-import Logout from './Logout'
+import Login from './Login.js'
+import Logout from './Logout.js'
 // import { NavLink } from 'react-router-dom'
 
 //Functional Component:
 const NavBar = ({ currentUser }) => {
-  return (
-    <div className="NavBar">
-        { currentUser ? `Welcome, ${currentUser.name}` : "" }
-       { currentUser ? <Logout /> : <Login /> }
-      {/* { loggedIn ? <><p id="loggedin">Logged in as {currentUser.attributes.name}</p><Logout/></> : null} */}
-    </div>
-  )
+    return (
+        <div className="NavBar">
+            {currentUser ? `Welcome, ${currentUser.name}` : ""}
+            {currentUser ? <Logout /> : <Login />}
+        </div>
+    )
 }
 
 // I can do this because I know the incoming argument is an object, state, coming from redux
@@ -24,10 +23,9 @@ const NavBar = ({ currentUser }) => {
 //The incoming argument (state) is an object, coming from Redux, therefore we can discontruct it and grab its currentUser property
 //Allow App.js to have knowledge of the current user, to show the Login button only if user is logged out and vice versa
 const mapStateToProps = ({ currentUser }) => {
-  return {
-    currentUser
-    // loggedIn: !!currentUser
-  }
+    return {
+        currentUser
+    } 
 }
 
 export default connect(mapStateToProps)(NavBar)
