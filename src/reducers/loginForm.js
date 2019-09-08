9019434a460ae495677fb08d3d6524bee171083a
Wendy Calderon <wendycalderon@gmail.com>
2019-09-08 14:53:57 -0400

    Add case statement to reset the form

--- src/reducers/loginForm.js
+++ src/reducers/loginForm.js
@@ -7,6 +7,9 @@ const initialState = {
     switch (action.type) {
       case "UPDATE_LOGIN_FORM":
         return action.formData
+      case "RESET_LOGIN_FORM":
+        // we want to return the initialState, meaning a clear form
+        return initialState
       default:
         return state
     }
