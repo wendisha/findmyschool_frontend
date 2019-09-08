b3dc45ca1812462be88e9dcc52efb606e995ecf2
Wendy Calderon <wendycalderon@gmail.com>
2019-09-08 16:27:04 -0400

    Import myBookmarks reducer

--- src/store.js
+++ src/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import usersReducer from './reducers/users.js'
 import currentUser from './reducers/currentUser.js'
+import myBookmarks from './reducers/myBookmarks.js'
 import loginForm from './reducers/loginForm.js'
 import thunk from 'redux-thunk'
 
 //Displaying different syntax; could've user currentUser: currentUser
-const reducer = combineReducers({ user: usersReducer, currentUser, loginForm });
+const reducer = combineReducers({ users: usersReducer, currentUser, loginForm, myBookmarks });
 
 // Newer way of setting up middleware
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
