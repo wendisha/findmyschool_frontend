83b0d590a18c56e0f46d073a4fc4c80659f6f1f5
Wendy Calderon <wendycalderon@gmail.com>
2019-09-11 17:37:34 -0400

    Add getMyBookmarks action creator

--- src/actions/myBookmarks.js
+++ src/actions/myBookmarks.js
@@ -4,3 +4,24 @@ export const setMyBookmarks = bookmarks => {
       bookmarks
     }
 }
+
+export const getMyBookmarks = () => {
+  return dispatch => {
+    return fetch("http://localhost:3001/api/v1/bookmarks", {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+      .then(r => r.json())
+      .then(r => {
+        if (r.error) {
+          alert(response.error)
+        } else {
+          dispatch(setMyBookmarks(r.data))
+        }
+      })
+      .catch(console.log)
+  }
+}
