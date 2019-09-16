43fbc79a357ea3853ad4cbf7eb4b7cc42456dbaf
Wendy Calderon <wendycalderon@gmail.com>
2019-09-16 18:07:19 -0400

    Use withRouter to fix route issue after logout

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { logout } from "../actions/currentUser.js"
+import { withRouter } from 'react-router-dom'
 
 //props get passed into a functional component as an object argument
-const Logout = ({ logout }) => {
+const Logout = ({ logout, history  }) => {
 
     // const handleSubmit = event => {
         
     // }
 
     return (
-        <form onSubmit={logout}>
-          <input type="submit" value="Log Out"/>
-        </form>
+      <form onSubmit={ (event) => {
+          event.preventDefault()
+          logout()
+          history.push('/')
+          }
+        }>
+        <input type="submit" value="Log Out"/>
+      </form>
     )
 }
 
 //Connect is a function that takes up to 4 arguments, and returns a function that takes a component and returns a component
 //If I dont need mapstatetoprops as my first argument, I need to reference it as null to avoid it seeing it as mapstatetoprops
-export default connect(null, { logout })(Logout)
+export default withRouter(connect(null, { logout } )(Logout))
