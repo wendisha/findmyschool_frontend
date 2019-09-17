17809a57f4d62b7798ae1fd93e3fa9837b6dab96
Wendy Calderon <wendycalderon@gmail.com>
2019-09-17 19:53:26 -0400

    Add navlink to Browse Schools

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,6 +14,7 @@ const NavBar = ({ currentUser, loggedIn }) => {
         //DELETE BOOKMARKS FROM HERE!!!!
         <div className="NavBar">
             <NavLink exact activeClassName="active" to="/bookmarks">My Bookmarks</NavLink>
+            <NavLink exact activeClassName="active" to="/browse-schools">Browse Schools</NavLink>
             {/* {currentUser ? `Welcome, ${currentUser.attributes.name}` : ""}  */}
             {/* <button>Log In</button> Or <button>Sign Up</button> */}
             {/* {currentUser ? <Logout /> : <Login />} */}
