8e90130841b3364d0d6bfdc44155125bcb67c9ee
Wendy Calderon <wendycalderon@gmail.com>
2019-09-20 10:02:06 -0400

    Work on handleClick

--- src/components/SchoolCard.js
+++ src/components/SchoolCard.js
@@ -1,6 +1,10 @@
 import React from 'react'
 // import {Link} from 'react-router-dom'
 
+const handleClick = () => {
+  addSchoolToBookmarks
+}
+
 const SchoolCard = ({ school }) => {
   return ( 
     <div key={school.id}>
@@ -9,9 +13,20 @@ const SchoolCard = ({ school }) => {
       <p>{school.location.city}, {school.location.state}  |  {school.phone} </p>
       <img src={school.image_url} alt={`School: ${school.name}`}/><br/><br/>
       <a target="_blank" href={school.url}>Learn more about this school</a><br/><br/>
-      <button>Bookmark this school</button>
+      <button onClick={handleClick}>Bookmark this school</button>
     </div> 
   )
 }
 
+// const handleClick = props => <button onClick={props.onClick}></button>
+
 export default SchoolCard
+
+
+
+// Like for a stateless component, just add a function -
+
+// function addName(){
+//    console.log("name is added")
+// }
+// and it is called in the return as onChange={addName}
