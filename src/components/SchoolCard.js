f6b389b9729d7fc35760d7f55df5e82b71c0e2c1
Wendy Calderon <wendycalderon@gmail.com>
2019-09-19 22:44:38 -0400

    Add image, city and phone

--- src/components/SchoolCard.js
+++ src/components/SchoolCard.js
@@ -2,7 +2,13 @@ import React from 'react'
 // import {Link} from 'react-router-dom'
 
 const SchoolCard = ({ school }) => {
-  return ( <p>{school.name}</p> )
+  return ( 
+    <div key={school.id}>
+      <img src={school.image_url} alt={`School: ${school.name}`}/>
+      <h1>{school.name}</h1>
+      <p>{school.location.city} | {school.phone} </p>
+    </div> 
+  )
 }
 
 export default SchoolCard
