17415e028f5420b1b4b31531451c3d907f847d1d
Wendy Calderon <wendycalderon@gmail.com>
2019-09-20 10:44:39 -0400

    Add key and delete commented out code

--- src/components/ListSchools.js
+++ src/components/ListSchools.js
@@ -5,20 +5,8 @@ import SchoolCard from './SchoolCard';
 
 // THIS COMPONENT DISPLAY THE RESULTS OF DAYCARES' SEARCH
 const ListSchools = (props) => {
-
-    // const renderSchoolsList = () => {
-    //     return props.schoolsList.map((school) => {
-    //         return (                               
-    //             <div key={school.id}>
-    //                 <img src={school.image_url} alt={`School: ${school.name}`}/>
-    //                 <h1>{school.name}</h1>
-    //                 <p>{school.location.city} | {school.phone} </p>
-    //             </div>           
-    //         );
-    //     })
-    // }
     const renderSchoolsList = () => {
-        const schoolCards = props.schoolsList.length > 0 ? props.schoolsList.map(s => <SchoolCard school={s} />) : null
+        const schoolCards = props.schoolsList.length > 0 ? props.schoolsList.map(s => <SchoolCard school={s} key={s.id}/>) : null
         return (schoolCards)
     }
 
