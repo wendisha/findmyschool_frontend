b1faae465563403c0bc0d7d12254356c630d6018
Wendy Calderon <wendycalderon@gmail.com>
2019-09-20 09:42:31 -0400

    Delete commented out code

--- src/reducers/browseSchoolsForm.js
+++ src/reducers/browseSchoolsForm.js
@@ -9,20 +9,3 @@ export default (state = initialState, action) => {
             return state
     }
 }
-
-// export default (state = initialState, action) => {
-//   switch (action.type) {
-//       case "LIST_SCHOOLS":
-//           return {...state, schools: state.schools.concat(action.payload)}
-//       default:
-//           return state
-//   }
-// }
-
-
-// return {
-//     ...state,
-//     breakfastBowls: action.saladBowls.filter(saladBowl => saladBowl.meal_time_frame === 'breakfast'),
-//     lunchBowls: action.saladBowls.filter(saladBowl => saladBowl.meal_time_frame === 'lunch'),
-//     dinnerBowls: action.saladBowls.filter(saladBowl => saladBowl.meal_time_frame === 'dinner')
-//   }
