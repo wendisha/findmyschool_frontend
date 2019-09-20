a4d7f3bf3d16ba69647725e4bf8a146ee089edaa
Wendy Calderon <wendycalderon@gmail.com>
2019-09-20 18:46:35 -0400

    Comment out unnused code

--- src/components/MyBookmarks.js
+++ src/components/MyBookmarks.js
@@ -10,16 +10,9 @@ const MyBookmarks = props => {
     )
 }
 
-
 //We call this function mapStateToProps by convention, so that everyone knows what it is doing, but it could be named whatever
 //Here we grab our Bookmarks array from the state of the store!!!
 //And that's exactly why we need this function, because we need access to our redux store
-
-//Howard: 
-// we provide mapStateToProps to Redux in order to tell Redux:
-// "Excuse me Redux, would you please provide use access to your state
-// so that we may pick and choose the pieces of state we would like availble
-// to this particular component as props."
 const mapStateToProps = state => {
     return {
       bookmarks: state.myBookmarks
