b5a0efebdca9247d032832c051883db982f62299
Wendy Calderon <wendycalderon@gmail.com>
2019-09-20 10:46:42 -0400

    Delete commented out code

--- src/actions/browseSchools.js
+++ src/actions/browseSchools.js
@@ -29,29 +29,4 @@ export const fetchSchools = (browseFormData) => {
         })
         .catch(console.log)  
       }
-
-      // const proxyurl = "https://cors-anywhere.herokuapp.com/"
-      // const url = `https://api.yelp.com/v3/businesses/search?term=school&location=${browseFormData.zipcode}&limit=1`
-      // return  (
-      //     fetch(proxyurl + url,  {
-      //         method: 'GET',
-      //         headers: {
-      //             'Accept': 'application/json',
-      //             'Authorization': process.env.REACT_APP_YELP_API_KEY,
-      //             'Content-Type': 'application/json'
-      //         }
-      //     }).then(r => r.json())
-      //     .then(data => {
-      //       console.log("fetch working", data)
-      //       if (data.error) {
-      //         alert(data.error)
-      //       } else {
-      //         // dispatch(listSchools(data))
-      //       }
-      //     })
-      //     .catch(console.log)  
-        // )
 }
-
-
-//CHANGE API WOMANNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
