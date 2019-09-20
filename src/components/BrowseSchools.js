d31f1ca58d20d26f4bceebbca94590dd9725ca62
Wendy Calderon <wendycalderon@gmail.com>
2019-09-19 21:27:18 -0400

    Fix proper rendering of fetch results

--- src/components/BrowseSchools.js
+++ src/components/BrowseSchools.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux';
 import { fetchSchools } from '../actions/browseSchools.js';
+// import SchoolCard from './SchoolCard' 
+import ListSchools from './ListSchools'
 
 class BrowseSchools extends React.Component {
    
@@ -10,8 +12,14 @@ class BrowseSchools extends React.Component {
 
      handleSubmit = event => {
         event.preventDefault()
-        this.props.fetchSchools(this.state)
-        // console.log("hi wendy",this.state)
+        this.props.fetchSchools(this.state) 
+
+        // console.log('INSIDE THE FETCH HANDLE-SUBMIT') // 1
+        // console.log(this.props.entireState) // 2
+        // console.log('BUT AFTER THE entireState') // 3
+        // console.log(this.props.schoolsList)
+           
+        // this.props.schoolsList.forEach( (school) => console.log(school))
 
         // this.setState({
         //     email: "",
@@ -20,8 +28,8 @@ class BrowseSchools extends React.Component {
         // };
       //reset the React state after submit!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     
-        // we have the react state that we need to pass into afucntion that will fire a get request to an api--  this.state
      }
+
 
     
     onChange = (event) => {
@@ -29,22 +37,42 @@ class BrowseSchools extends React.Component {
           [event.target.name]: event.target.value
         })
     }
+
+    // listSchools = props => {
+    //     const schoolCards = props.schoolsList.length > 0 ? props.schoolsList.map(s => <SchoolCard school={s} key={s.id}/>) : null
+    //     console.log(schoolCards)
+    //     return (
+    //         schoolCards
+    //     )
+    //     // console.log(schoolCards)
+    // }
+
     
     render (){
         return (     
+            <> 
             <form onSubmit={ this.handleSubmit }>
                 <h1>Enter zipcode to browse schools:</h1>
                 <input placeholder="zipcode" type="text" name="zipcode" onChange={this.onChange}/>
-                {/* <input placeholder="city" type="text" name="city" onChange={this.onChange}/> */}
                 <input type="submit" value="Browse"/>
             </form>
+            <ListSchools />
+            {/* <div className="schools">
+                {console.log(this.props.schoolsList)}
+                {console.log(this.props.entireState)}
+            </div> */}
+            </>
         )   
     }   
 }
 
+// {/* <> </> is called fragment */}
+
 const mapStateToProps = state => {
     return {
-        browseFormData: state.browseSchools
+        browseFormData: state.browseSchoolsForm,
+        schoolsList: state.browseSchoolsForm,
+        entireState: state
     }
 }
 
