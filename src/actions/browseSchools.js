29a630c37b7405b14259781e85370a3473b48a34
Wendy Calderon <wendycalderon@gmail.com>
2019-09-19 21:27:02 -0400

    Fix proper rendering of fetch results

--- src/actions/browseSchools.js
+++ src/actions/browseSchools.js
@@ -9,7 +9,7 @@ export const fetchSchools = (browseFormData) => {
     const proxyurl = "https://cors-anywhere.herokuapp.com/"
     const url = `https://api.yelp.com/v3/businesses/search?term=school&location=${browseFormData.zipcode}&limit=5`
     return  dispatch => {
-      console.log(browseFormData)
+      // console.log(browseFormData)
         fetch(proxyurl + url,  {
             method: 'GET',
             headers: {
@@ -19,10 +19,11 @@ export const fetchSchools = (browseFormData) => {
             }
         }).then(r => r.json())
         .then(data => {
-          console.log("fetch working", data)
+          // console.log("fetch working", data)
           if (data.error) {
             alert(data.error)
           } else {
+            // console.log(data)
             dispatch(listSchools(data))
           }
         })
