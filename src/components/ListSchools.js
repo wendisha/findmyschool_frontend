593b1d92576776d94c7d9e292cc75c73ec92d60b
Wendy Calderon <wendycalderon@gmail.com>
2019-09-19 22:44:09 -0400

    Refactor to make use of SchoolCard

--- src/components/ListSchools.js
+++ src/components/ListSchools.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// import BrowseSchools from './BrowseSchools';
+import SchoolCard from './SchoolCard';
 
 
 // THIS COMPONENT DISPLAY THE RESULTS OF DAYCARES' SEARCH
 const ListSchools = (props) => {
 
-    const renderSchools = () => {
-        return props.schoolsList.map((school) => {
-            return (                               
-                <div key={school.id}>
-                    <img src={school.image_url} alt={`School: ${school.name}`}/>
-                    <h1>{school.name}</h1>
-                    <p>{school.location.city} | {school.phone} </p>
-                </div>           
-            );
-        })
+    // const renderSchoolsList = () => {
+    //     return props.schoolsList.map((school) => {
+    //         return (                               
+    //             <div key={school.id}>
+    //                 <img src={school.image_url} alt={`School: ${school.name}`}/>
+    //                 <h1>{school.name}</h1>
+    //                 <p>{school.location.city} | {school.phone} </p>
+    //             </div>           
+    //         );
+    //     })
+    // }
+    const renderSchoolsList = () => {
+        const schoolCards = props.schoolsList.length > 0 ? props.schoolsList.map(s => <SchoolCard school={s} />) : null
+        return (schoolCards)
     }
 
     return (  
         <div>  
             <ul>
-                {renderSchools()}
+                {renderSchoolsList()}
             </ul>
         </div> 
     )
