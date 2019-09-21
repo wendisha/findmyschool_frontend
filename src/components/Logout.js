d38f0d3c7d612af3a3d471401ad2ad834068a323
Wendy Calderon <wendycalderon@gmail.com>
2019-09-21 16:39:25 -0400

    Apply Bootstrap to Logout button

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { logout } from "../actions/currentUser.js"
 import { withRouter } from 'react-router-dom'
+import { Navbar, Nav , Form, Button } from 'react-bootstrap';
 
 //props get passed into a functional component as an object argument
 const Logout = ({ logout, history  }) => {
@@ -11,16 +12,26 @@ const Logout = ({ logout, history  }) => {
     // }
 
     return (
-      <form onSubmit={ (event) => {
+      <Form onSubmit={ (event) => {
+        event.preventDefault()
+        logout()
+        history.push('/')
+        }
+      }>
+      {/* <form onSubmit={ (event) => {
           event.preventDefault()
           logout()
           history.push('/')
           }
-        }>
-        <input type="submit" value="Log Out"/>
-      </form>
+        }> */}
+          <Button variant="outline-primary" type="submit">Log Out</Button>
+        {/* <input type="submit" value="Log Out"/> */}
+      {/* </form> */}
+      </Form>
     )
 }
+
+
 
 //Connect is a function that takes up to 4 arguments, and returns a function that takes a component and returns a component
 //If I dont need mapstatetoprops as my first argument, I need to reference it as null to avoid it seeing it as mapstatetoprops
