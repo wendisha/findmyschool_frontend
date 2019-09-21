f2210f39d269a7927972435f50752b919615bae6
Wendy Calderon <wendycalderon@gmail.com>
2019-09-21 17:13:02 -0400

    Clean up code

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,36 +2,21 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { logout } from "../actions/currentUser.js"
 import { withRouter } from 'react-router-dom'
-import { Navbar, Nav , Form, Button } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 
-//props get passed into a functional component as an object argument
+//Props get passed into a functional component as an object argument
 const Logout = ({ logout, history  }) => {
-
-    // const handleSubmit = event => {
-        
-    // }
-
-    return (
-      <Form onSubmit={ (event) => {
-        event.preventDefault()
-        logout()
-        history.push('/')
-        }
-      }>
-      {/* <form onSubmit={ (event) => {
-          event.preventDefault()
-          logout()
-          history.push('/')
-          }
-        }> */}
-          <Button variant="outline-primary" type="submit">Log Out</Button>
-        {/* <input type="submit" value="Log Out"/> */}
-      {/* </form> */}
-      </Form>
-    )
+  return (
+    <Form onSubmit={ (event) => {
+      event.preventDefault()
+      logout()
+      history.push('/')
+      }}>
+          
+      <Button variant="outline-light" type="submit">Log Out</Button>
+    </Form>
+  )
 }
-
-
 
 //Connect is a function that takes up to 4 arguments, and returns a function that takes a component and returns a component
 //If I dont need mapstatetoprops as my first argument, I need to reference it as null to avoid it seeing it as mapstatetoprops
