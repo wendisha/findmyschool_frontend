156c26580464c0222bccf2c0eb1d4ad0be41310b
Wendy Calderon <wendycalderon@gmail.com>
2019-09-21 23:20:21 -0400

    Format form with bootstrap

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { updateSignupForm } from "../actions/signupForm.js"
 import { signup } from "../actions/currentUser.js"
+import { Button, Form, Container } from 'react-bootstrap';
 
 const Signup = ({ signupFormData, updateSignupForm, history, signup }) => {
 
@@ -20,13 +21,25 @@ const Signup = ({ signupFormData, updateSignupForm, history, signup }) => {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-          <input placeholder="name" value={signupFormData.name} name="name" type="text" onChange={handleUserInfoInputChange} />
-          <input placeholder="username" value={signupFormData.username} name="username" type="text" onChange={handleUserInfoInputChange} />
-          <input placeholder="password" value={signupFormData.password} name="password" type="text" onChange={handleUserInfoInputChange} />
-          <input type="submit" value="Sign Up"/>
-        </form>
-    )
+      <Container expand='md'>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="formBasicName">
+            <Form.Label>Name:</Form.Label>
+            <Form.Control type="text" value={signupFormData.name} name="name" placeholder="Enter name" onChange={handleUserInfoInputChange}/>
+            <Form.Text className="text-muted">We'll never share your information with anyone else.</Form.Text>
+          </Form.Group>
+          <Form.Group controlId="formBasicUsername">
+            <Form.Label>Username:</Form.Label>
+            <Form.Control type="text" value={signupFormData.username} name="username" placeholder="Enter username" onChange={handleUserInfoInputChange}/>
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control type="password" value={signupFormData.password} name="password" placeholder="Enter password" onChange={handleUserInfoInputChange}/>
+        </Form.Group>
+        <Button variant="primary" type="submit" value="Sign Up">Sign Up</Button>
+      </Form>
+    </Container>
+  )
 }
 
 const mapStateToProps = state => {
