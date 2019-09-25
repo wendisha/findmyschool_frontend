5f12b39dafe8be3753c6a874e78edc67b4eabdaf
Wendy Calderon <wendycalderon@gmail.com>
2019-09-24 20:38:31 -0400

    Add comment

--- src/components/BookmarkCard.js
+++ src/components/BookmarkCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+//Props get passed into a functional component as an object argument
 const BookmarkCard = ({ bookmark }) => {
   return ( <p>{bookmark.attributes.school_name}</p> )
 }
