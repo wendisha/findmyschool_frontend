410b8569a1528502b769c13d035112978143e1d9
Wendy Calderon <wendycalderon@gmail.com>
2019-09-24 20:50:32 -0400

    Clean up code

--- src/reducers/signupForm.js
+++ src/reducers/signupForm.js
@@ -5,13 +5,13 @@ const initialState = {
     password: "",
   }
   
-  export default (state=initialState, action) => {
-    switch (action.type) {
-      case "UPDATE_SIGNUP_FORM":
-        return action.formData
-      case "RESET_SIGNUP_FORM":
-        return initialState
-      default:
-        return state
-    }
+export default (state=initialState, action) => {
+  switch (action.type) {
+    case "UPDATE_SIGNUP_FORM":
+      return action.formData
+    case "RESET_SIGNUP_FORM":
+      return initialState
+    default:
+      return state
   }
+}
