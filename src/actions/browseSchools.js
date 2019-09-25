dcbeae4611bdf691cc1471af2a9eb166b565d793
Wendy Calderon <wendycalderon@gmail.com>
2019-09-24 20:17:47 -0400

    Clean code

--- src/actions/browseSchools.js
+++ src/actions/browseSchools.js
@@ -1,32 +1,30 @@
 export const listSchools = schools => {
     return {
-        type: "LIST_SCHOOLS",
-        schools //payload: schools
+        type: "LIST_SCHOOLS",       //label to allow the reducer to recognize what happened
+        schools                     //payload: schools, this is the data that we passed to the action from a component
     }
 }
 
 export const fetchSchools = (browseFormData) => {
-    const proxyurl = "https://cors-anywhere.herokuapp.com/"
-    const url = `https://api.yelp.com/v3/businesses/search?term=school&location=${browseFormData.zipcode}&limit=5`
-    return  dispatch => {
-      // console.log(browseFormData)
-        fetch(proxyurl + url,  {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Authorization': process.env.REACT_APP_YELP_API_KEY,
-                'Content-Type': 'application/json'
-            }
-        }).then(r => r.json())
-        .then(data => {
-          // console.log("fetch working", data)
-          if (data.error) {
-            alert(data.error)
-          } else {
-            // console.log(data)
-            dispatch(listSchools(data))
-          }
-        })
-        .catch(console.log)  
+  const proxyurl = "https://cors-anywhere.herokuapp.com/"
+  const url = `https://api.yelp.com/v3/businesses/search?term=school&location=${browseFormData.zipcode}&limit=5`
+  return  dispatch => {
+    fetch(proxyurl + url,  {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': process.env.REACT_APP_YELP_API_KEY,
+        'Content-Type': 'application/json'
+        }
+      })
+      .then(r => r.json())
+      .then(data => {
+      if (data.error) {
+        alert(data.error)
+      } else {
+        dispatch(listSchools(data))
       }
+    })
+  .catch(console.log)  
+  }
 }
