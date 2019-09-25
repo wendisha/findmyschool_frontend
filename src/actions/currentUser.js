9c3b563abbf71579a204c08ead0230f1592785bd
Wendy Calderon <wendycalderon@gmail.com>
2019-09-24 20:23:02 -0400

    Clean up code

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -2,11 +2,11 @@ import { resetLoginForm } from "./loginForm.js"
 import { resetSignupForm } from "./signupForm.js"
 import { getMyBookmarks, clearBookmarks } from "./myBookmarks.js"
 
-//SYNCHRONOUS ACTION CREATORS: (synchronous action creators return POJOS (plain old js objects))
+//SYNCHRONOUS ACTION return POJOS (plain old js objects)
 export const setCurrentUser = user => {
     return {
         type: "SET_CURRENT_USER",
-        user //payload: user
+        user
     }
 }
 
@@ -16,14 +16,11 @@ export const clearCurrentUser = () => {
     }
 }
 
-//ASYNCHRONOUS ACTION CREATORS:
-//this is a asynchronous action, we need to send a request to our back end in order to log in
-//Impt! Thunk gives us the ability to return an action creator in the form of a function! So our return is the same as saying return function (dispatch) { }
+//Impt! Thunk gives us the ability to return an action creator in the form of a function! 
+//So our return is the same as saying return function (dispatch) { }
 export const login = (credentials, history) => {
     return dispatch => {
-        // localhost:3001 Rails Server 
         return fetch("http://localhost:3001/api/v1/login", {
-            //post, because we're setting data about an user in the db
             credentials: "include",
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -47,27 +44,27 @@ export const login = (credentials, history) => {
 
 export const signup = (credentials, history) => {
     return dispatch => {
-      const userInfo = {
-        user: credentials
-      }
-      return fetch("http://localhost:3001/api/v1/signup", {
-        credentials: "include",
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(userInfo)
-      })
+        const userInfo = {
+            user: credentials
+        }
+        return fetch("http://localhost:3001/api/v1/signup", {
+            credentials: "include",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(userInfo)
+        })
         .then(r => r.json())
         .then(r => {
-          if (r.error) {
-            alert(r.error)
-          } else {
-            dispatch(setCurrentUser(r.data))
-            dispatch(getMyBookmarks())
-            dispatch(resetSignupForm())
-            history.push('/')
-          }
+            if (r.error) {
+                alert(r.error)
+            } else {
+                dispatch(setCurrentUser(r.data))
+                dispatch(getMyBookmarks())
+                dispatch(resetSignupForm())
+                history.push('/')
+            }
         })
         .catch(console.log)
     }
@@ -102,7 +99,7 @@ export const logout = () => {
         dispatch(clearCurrentUser())
         dispatch(clearBookmarks())
         return fetch('http://localhost:3001/api/v1/logout', {
-            //sends cookies back:
+            //Sends cookies back:
             credentials: "include",
             method: "DELETE"
         })
