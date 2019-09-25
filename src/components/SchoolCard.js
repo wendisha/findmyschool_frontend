ad3cd267784d49cd0e3a5ead76480e24d83b2a6f
Wendy Calderon <wendycalderon@gmail.com>
2019-09-24 21:01:28 -0400

    Delete braket

--- src/components/SchoolCard.js
+++ src/components/SchoolCard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux';
-import { addSchoolToBookmarks } from '../actions/myBookmarks']
+import { addSchoolToBookmarks } from '../actions/myBookmarks'
 
 const SchoolCard = ({ school, addSchoolToBookmarks }) => {
 
