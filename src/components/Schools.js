eb594f0d91c14dfa2eee1c9ce3f107a78678aed9
Wendy Calderon <wendycalderon@gmail.com>
2019-09-25 11:12:29 -0400

    Comment out unnecessary code

--- src/components/Schools.js
+++ src/components/Schools.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import SchoolCard from './SchoolCard' 
+// import React from 'react'
+// import { connect } from 'react-redux'
+// import SchoolCard from './SchoolCard' 
 
-//funtional container component:
-const Schools = props => {
-    const schoolCards = props.schools.length > 0 ? props.schools.map(s => <SchoolCard school={s} key={s.id}/>) : null
-    return (
-        schoolCards
-    )
-}
+// //funtional container component:
+// const Schools = props => {
+//     const schoolCards = props.schools.length > 0 ? props.schools.map(s => <SchoolCard school={s} key={s.id}/>) : null
+//     return (
+//         schoolCards
+//     )
+// }
 
-const mapStateToProps = state => {
-    return {
-      schools: state.schools
-    }
-}
+// const mapStateToProps = state => {
+//     return {
+//       schools: state.schools
+//     }
+// }
 
-export default connect(mapStateToProps)(Schools)
+// export default connect(mapStateToProps)(Schools)
