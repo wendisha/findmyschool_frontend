a891e4d175c0043a36ce1bd7c346e408808667b7
Wendy Calderon <wendycalderon@gmail.com>
2019-09-24 20:50:25 -0400

    Clean up code

--- src/reducers/browseSchoolsForm.js
+++ src/reducers/browseSchoolsForm.js
@@ -3,7 +3,7 @@ const initialState = []
 export default (state = initialState, action) => {
     switch (action.type) {
         case "LIST_SCHOOLS":
-            //console.log(action.schools.businesses)
+            //the reducer updates the global store with the data that was sent by action
             return action.schools.businesses  // action.schools
         default:
             return state
