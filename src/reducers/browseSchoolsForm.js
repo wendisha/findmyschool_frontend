25944ba879e4cd0bf9ae39ab8e0fab6475a6b3e8
Wendy Calderon <wendycalderon@gmail.com>
2019-09-25 11:13:26 -0400

    Add clear schools

--- src/reducers/browseSchoolsForm.js
+++ src/reducers/browseSchoolsForm.js
@@ -5,6 +5,8 @@ export default (state = initialState, action) => {
         case "LIST_SCHOOLS":
             //the reducer updates the global store with the data that was sent by action
             return action.schools.businesses  // action.schools
+        case "CLEAR_SCHOOLS":
+            return []
         default:
             return state
     }
